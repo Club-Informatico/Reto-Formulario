02d6add1a8edaafdd74f2ec70c85d416e1324ddc
G4os <gustavorellana700@gmail.com>
2025-09-21 23:51:46 -0300

    mejorando form y agregando expresion regular del email

--- gustavo-orellana/static/script.js
+++ gustavo-orellana/static/script.js
@@ -1,6 +1,17 @@
 function validarRut (rut) {
     return /^(\d{7,8}-[\dkK])$|^(\d{1,2}\.\d{3}\.\d{3}-[\dkK])$/.test(rut);
 }
+
+function validarEmail(email) {
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+}
+
+console.log(validarEmail("usuario@correo.cl"));      
+console.log(validarEmail("usuario.correo@dominio.com")); 
+console.log(validarEmail("usuario@correo"));         
+console.log(validarEmail("usuario@.com"));           
+console.log(validarEmail("usuario@correo.c"));       
+
 console.log(validarRut("12333555-7"))
 console.log(validarRut("123335557"))
 console.log(validarRut("1233355a7"))
